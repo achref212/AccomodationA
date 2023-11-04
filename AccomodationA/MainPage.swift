6c4438604ec5b692cda4aeb42ae742f3452f1d4d
achref212 <achref.chaabani@esprit.tn>
2023-11-04 20:51:03 +0100

    first interface

--- AccomodationA/MainPage.swift
+++ AccomodationA/MainPage.swift
@@ -1,86 +1,146 @@
-//
-//  ContentView.swift
-//  AccomodationA
-//
-//  Created by chaabani achref on 3/11/2023.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    struct ImageInfo: Identifiable, Hashable  {
+        var id = UUID()
+        var imageName: String
+        var title: String
+        var location: String
+        var price: String
+        var racing: String
+    }
+    var imageNames = ["home1", "home2", "home3"]
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var imageInfoList: [ImageInfo] = [
+        ImageInfo(imageName: "home1", title: "home desc", location: "Paris, France", price: "$299 / day", racing: "5.0"),
+        ImageInfo(imageName: "home2", title: "home2 desc", location: "Paris, France", price: "$299/ day", racing: "5.0"),
+        ImageInfo(imageName: "home3", title: "home3 desc", location: "Paris, France", price: "$299/ day", racing: "5.0"),
+    ]
+
+    @State private var selectedCategory: String = "Recommended"
+
+    var categories = ["Recommended", "Popular", "Trending"]
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ScrollView {
+            VStack {
+                HStack {
+                    Image("logo1")
+                        .resizable()
+                        .frame(width: 60, height: 70) // Ajustez la taille selon vos besoins
+                    
+                    Text("Hello, User!")
+                        .font(.title)
+                        .padding()
+                }
+
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .padding(.leading, 10)
+
+                    TextField("Rechercher...", text: .constant(""))
+                        .padding(.vertical, 10)
+                        .padding(.trailing, 10)
+                }
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .padding()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        ForEach(categories, id: \.self) { category in
+                            Text(category)
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 10)
+                                .frame(width:180,height:50)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(selectedCategory == category ? Color.blue : Color.clear)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .stroke(selectedCategory == category ? Color.clear : Color.blue, lineWidth: 1)
+                                        )
+                                )
+                                .foregroundColor(selectedCategory == category ? .white : .blue)
+                                .onTapGesture {
+                                    selectedCategory = category
+                                }
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(imageNames, id: \.self) { imageName in
+                            VStack {
+                                Image(imageName)
+                                    .resizable()
+                                    .frame(width: 150, height: 100)
+                                    .cornerRadius(10)
+
+                                if let info = imageInfoList.first(where: { $0.imageName == imageName }) {
+                                    Text(info.title)
+                                        .padding(.vertical, 5)
+                                    Text(info.location)
+                                        .padding(.vertical, 5)
+                                    Text(info.price)
+                                        .padding(.vertical, 5)
+                                }
+                            }
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(10)
+                        }
                     }
+                    .padding()
                 }
-            }
-            Text("Select an item")
-        }
-    }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                
+                    .padding()
+                ScrollView(.vertical, showsIndicators: false) {
+                   
+                        ForEach(imageInfoList, id: \.self) { info in
+                            if selectedCategory == "Recommended" || selectedCategory == info.title {
+                                VStack {
+                                    Image(info.imageName)
+                                        .resizable()
+                                        .frame(width: 300, height: 200)
+                                        .cornerRadius(10)
+                                    
+                                    Text(info.title)
+                                        .font(.title)
+                                        .padding()
+                                    
+                                    Text(info.location)
+                                        .padding()
+                                    
+                                    Text(info.price)
+                                        .padding()
+                                    
+                                    HStack {
+                                        Image(systemName: "bookmark")
+                                            .padding()
+                                        
+                                        // Ajoutez votre système de notation ici (rating)
+                                        Text("Rating: \(info.racing)")
+                                            .padding()
+                                    }
+                                }
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                                .padding(.bottom, 10)
+                            }
+                        }
+                    
+                }
+                .padding()
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
