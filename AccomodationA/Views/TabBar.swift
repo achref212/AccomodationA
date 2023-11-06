35ee284d7a1c1b1179069403cfab7af5b4c4cb03
achref212 <achref.chaabani@esprit.tn>
2023-11-06 11:32:15 +0100

    first  commit

---
+++ AccomodationA/Views/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  AccomodationA
+//
+//  Created by chaabani achref on 5/11/2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
